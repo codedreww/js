04bb5a5c4f810aec512ac66f7f7154995a0dab3c
Andrew Tan <158607852+codedreww@users.noreply.github.com>
2025-05-22 18:35:54 -0600

    22 May, first lecture, learned mostly about variables

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,21 @@
+/*
+naming conventions and variables
+
+let js = 'amazing';
+console.log(40 + 8 + 23 - 10); // console.log is used to print to the developer console.
+
+let firstName = 'Andrew';
+console.log(firstName);
+let PI = 3.1415; // All uppercase variable names are constants.
+let myFirstJob = 'Programmer';
+let myCurrentJob = 'Teacher';
+
+let Country = 'Indonesia';
+let Continet = 'Asia';
+console.log(Country);
+console.log(Continet);
+*/
+
+
+true;
+console.log(true); // true is a boolean value
