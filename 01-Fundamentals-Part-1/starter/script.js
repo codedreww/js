bfa3503151c0a0e8c93874e8cb326f682a3485e7
30207861 <andrew.tan1@ucalgary.ca>
2025-06-26 22:21:51 -0600

    26 june

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -15,7 +15,80 @@ let Continet = 'Asia';
 console.log(Country);
 console.log(Continet);
 */
-
-
-true;
+////////////////////////////////////////////////////
+/*
 console.log(true); // true is a boolean value
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof true);
+console.log(typeof 23);
+console.log(typeof 'Jonas');
+
+javascriptIsFun = "Yes!"; // we can re-assign a new value of an existing variable.
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null); // will give out an object in the console ( bug ).
+*/
+///////////////////////////////////////////////////
+/*
+// first way of declaring a variable : let
+let age = 30;
+age = 31; // we mutate the age variable.
+
+// second way of declaring a variable : const
+const PI = 3.14; // const is a constant, we cannot change that
+const birthYear = 1991; // We also need an initial value when using const. 
+birthYear = 1990; // will give out an error. 
+
+// third way of declaring a variable : var ( WE SHOULD NOT USE THIS !)
+// it is kinda similar to let, but it is different. it is the old way of using js, before let. 
+
+var job = "programmer";
+job = "teacher"; 
+
+// we can also just do something like this:
+lastName = "Tan";
+console.log(lastName);
+// but this is not a good practice, since js will not declare it in the program scope.
+*/
+////////////////////////////////////////////////////
+/*
+const now = 2037;
+const ageJonas = now - 1991; // minus operator
+const ageSarah = now - 2018;
+console.log(ageJonas, ageSarah);
+
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3); // multiplication , division and power. 
+// 2 ** 3 means 2 ^ 3
+
+const firstName = "Andrew";
+const lastName = "Tan";
+console.log(firstName + " " + lastName); // Concatenation of strings.
+
+//Assignment operators
+let x = 10 + 5;
+x += 10; // x = x + 10 = 25
+x *= 4; // x = x * 4 = 100
+x++; // x = x + 1
+x--;
+console.log(x);
+
+// Comparison operators
+console.log(ageJonas > ageSarah); // result will be boolean
+// >, <, >=, <=
+console.log(ageJonas >= 18);
+const isFullAge = ageSarah >= 18;
+
+console.log((now - 1991) > (now - 2018));
+*/
+
+///////////////////////////////////////////////////////
+
