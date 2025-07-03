66762b0f08f41c5a29fc843f5e21b3b1c1cc9ce3
30207861 <andrew.tan1@ucalgary.ca>
2025-07-03 02:11:39 -0600

    Finished chapter 1

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -91,4 +91,237 @@ console.log((now - 1991) > (now - 2018));
 */
 
 ///////////////////////////////////////////////////////
+// Operator Precedence
+/*
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+
+console.log(now - 1991 > now - 2018);
+
+let x, y;
+x = y = 25 - 10 - 5;
+console.log(x,y);
+
+const averageAge = (ageJonas + ageSarah) / 2;
+console.log(ageJonas, ageSarah, averageAge);
+*/
+/////////////////////////////////////////////////////
+
+// String and Tempelate Literals
+/*
+const firstName = 'Andrew';
+const job = 'teacher';
+const birthYear = 2004;
+const year = 2025;
+
+const andrew = "i'm " + firstName + ", a" + (year - birthYear) + " years old" + job + "!";
+console.log(andrew);
+
+// we use backticks ( `` ) for tempelate literals.
+const andrewNew = `I'm ${firstName}, a ${year - birthYear} years old ${job} !`;
+console.log(andrewNew)
+
+// we can use backticks for everything aswell if we want to make it easier. 
+
+console.log("String with \n\ multiple \n\ lines \n\ ");
+console.log(`String
+    with
+    multiple
+    lines`) // with tempelate literals, we can just use return instead of \n\ for multiple line strings
+
+*/
+////////////////////////////////////////////////////
+// if else
+
+/*
+const age = 12;
+const isOld = age >= 18;
+
+if(isOld){
+    console.log('Sarah can start driving license');
+} else{
+    console.log('Sarah is not able to start driving yet');
+    const yearsleft = 18 - age;
+    console.log(`Sarah needs to wait another ${yearsleft} years.`);
+}
+
+const birthYear = 1991;
+let century;
+if(birthYear <= 2000){
+    century = 20;
+} else{
+    century = 21;
+}
+
+console.log(century);
+*/
+////////////////////////////////////////////////////
+/*
+// type conversion : Manually convert one type to another
+// type coercion : JavaScript automatically convert the type for us behind the scenes
+
+// this is type conversion:
+const inputYear = '1991';
+console.log(inputYear + 18); // this will output 199118
+console.log(Number(inputYear) + 18); // using Number() will convert string to Number.
+
+console.log(typeof NaN); // will output a number lol.
+console.log(String(23)); // will become a string.
+
+// this is type coercion:
+console.log("I'm " + 23 + " years old.") // the number will be converted as a string. 
+console.log('23' - '10' - 3) // the strings will be converted to numbers.
+console.log('23' * '2') // the strings will also be converted to numbers since mult only works on numbers. 
+
+let n = '1' + 1;
+n = n - 1;
+console.log(n); // will output 10. 
+*/
+////////////////////////////////////////////////////
+
+// truthly and falsely values
+
+// 5 falsely values : 0, '', undefined, null, Nan
+// falsely values are values that will be false if converted to boolean
+
+// everything else is a truthly values
+// values that will be true if converted to boolean. 
+
+/*
+console.log(Boolean(0)); // false
+console.log(Boolean(NaN)); //false
+console.log(Boolean(undefined)); //false
+console.log(Boolean('Jonas')); // true
+console.log(Boolean('')); // false
+console.log(Boolean({})); //true
+
+const money = 0;
+if(money){ // this will be converted through boolean with type coercion. 
+    console.log(`Don't spend it all`);
+} else{
+    console.log(`You should get a job!`)
+}
+
+let height;
+if(height){
+    console.log('YAY!');
+} else{
+    console.log('Height is Undefined!')
+}
+
+*/
+///////////////////////////////////////////////
+// Equality operator
+
+/*
+const age = 18;
+if (age === 18) console.log("You just became an adult.");
+
+// using == will do type coercion
+if ('18' == 18) console.log("Javascript is mad wierd lol");
+// just assume this doesnt exist lol.
+
+const fav = Number(prompt("What's your favorite number?"));
+console.log(fav);
+console.log(typeof(fav));
+
+if(fav === 23) {
+    console.log("Cool i guess.");
+} else if(fav === 4){
+    console.log("not cool.");
+}
+
+if (favorite !== 23){
+    console.log("why not the 23?");
+}
+*/
+
+/////////////////////////////////////////////////
+// Boolean logic : the and, or, & not.
+
+/*
+const hasDriversLicense = true;
+const hasGoodVision = false;
+
+console.log(hasDriversLicense && hasGoodVision);
+console.log(hasDriversLicense || hasGoodVision);
+console.log(!hasDriversLicense);
+
+const shouldDrive = hasDriversLicense && hasGoodVision;
+if(shouldDrive) {
+    console.log(`Sarah should able to drive.`);
+} else{
+    console.log(`Sarah shouldn't drive yet.`);
+}
+
+const isNotTired = true;
+
+console.log(hasDriversLicense && hasGoodVision && isTired);
+*/
+/////////////////////////////////////////////////////
+
+//switch case
+
+/*
+const day = 'friday';
+
+switch(day){
+    case 'monday': // day === 'monday
+        console.log("today is monday.");
+        break; // we need a break here bcause if not, the code will continue executing without checking conditions. 
+    case 'tuesday':
+        console.log("not monday.");
+        break;
+    case 'wednesday':
+        console.log("not monday.");
+        break;
+    case 'thursday':
+    case 'friday':
+        console.log("today is either thursday or friday.");
+        break;
+    case 'saturday':
+    case 'friday':
+        console.log("is this the weekend?");
+        break;
+    default:
+        console.log("not a valid day.")
+}
+*/
+////////////////////////////////////////////////////
+// statemend vs expression
+
+/*
+// this is an expression : things that produce value such as boolean, etc. 
+3 + 4
+1991
+true && false && !false
+
+// this is statements : if else statement, switch case, etc.
+if (23 > 10){
+    const str = '23 is bigger.';
+}
+*/
+
+////////////////////////////////////////////////////
+// conditional operator
+/*
+const age = 23;
+age >= 18 ? console.log("I like to drink wine.") : console.log("I like to drink water.");
+// this is same as if age is more 18, like wine, else, like water.
+
+const drink = age >= 18 ? "wine" : "water";
+
+// same as code below.
+let drink2;
+if(age >= 18){
+    drink2 = "wine"
+} else{
+    drink2 = "water"
+}
+
+// we can use conditional operator on tempelate structure:
+console.log(`I like to drink ${age >= 18 ? "wine" : "water"}`);
+*/
+/////////////////////////////////////////////////////
 
