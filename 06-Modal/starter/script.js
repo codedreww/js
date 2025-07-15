68e69c0722c9d79bb65cc48087b152d9d092b5a9
Andrew Tan <158607852+codedreww@users.noreply.github.com>
2025-07-15 16:15:34 -0600

    Finished project 2

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,35 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+
+// to select multiple elements with the same class, we use querySelectorAll. querySelector will only use the first one.
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+const openModal = function () {
+  console.log('Button Clicked');
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+// using querySelectorAll will make the buttons inside a nodelist where we can use a forloop to access its content.
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener('click', openModal);
+}
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+// exit using escape key.
+document.addEventListener('keydown', function (e) {
+  // to check about the details of the event, we can do console.log(e) and check the output on a browser.
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
